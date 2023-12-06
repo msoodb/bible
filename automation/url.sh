bf40bb647c190225653a79c8db2ab8d1ade94244
msoodb <masoud.bolhassani@gmail.com>
2023-12-07 00:52:59 +0200

    tools sh tools

--- automation/url.sh
+++ automation/url.sh
@@ -8,11 +8,13 @@ TARGET=$1
 
 # waybackurls
 echo $TARGET | waybackurls  | tee -a urls~
+# waybackurls $TARGET | tee -a urls.wayback
+# cat urls.wayback | sort -u > urls.wayback.su
+# cat urls.wayback.su | grep -E "^http.?://www\.example\.com" > urls.wayback.su.inscope
+
 
 # katana
 katana -u $TARGET -fs=fqdn | tee -a urls~
 
 # pipes urls~ in a single file urls
 cat urls~ | sort -u > urls && rm urls~
-
-
